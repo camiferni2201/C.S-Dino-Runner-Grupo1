59ae4f4e640fc0ca6eed787b10266fd7681efcbe
camiferni2201 <112905170+camiferni2201@users.noreply.github.com>
2022-09-07 15:46:26 -0400

    Add files via upload

--- dino_runner/components/game.py
+++ dino_runner/components/game.py
@@ -1,7 +1,8 @@
 import pygame
 
-from dino_runner.utils.constants import BG, ICON, SCREEN_HEIGHT, SCREEN_WIDTH, TITLE, FPS
-
+from components.obstacle_manager import ObstacleManager
+from utils.constants import BG, ICON, SCREEN_HEIGHT, SCREEN_WIDTH, TITLE, FPS
+from components.dinosaur import Dinosaur 
 
 class Game:
     def __init__(self):
@@ -14,6 +15,8 @@ class Game:
         self.game_speed = 20
         self.x_pos_bg = 0
         self.y_pos_bg = 380
+        self.dinosaur = Dinosaur() ##Atributo para poder usar en los otros contextos
+        self.obstacle_manager = ObstacleManager()
 
     def run(self):
         # Game loop: events - update - draw
@@ -30,12 +33,16 @@ class Game:
                 self.playing = False
 
     def update(self):
-        pass
+        user_input = pygame.key.get_pressed()
+        self.dinosaur.update(user_input)
+        self.obstacle_manager.update()
 
     def draw(self):
         self.clock.tick(FPS)
         self.screen.fill((255, 255, 255))
         self.draw_background()
+        self.dinosaur.draw(self.screen)
+        self.obstacle_manager.draw(self.screen)
         pygame.display.update()
         pygame.display.flip()
 
